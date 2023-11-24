6730a144f732205c4bf6258fce2df9a5fa85cc4b
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-11-24 08:31:03 +0530

    Get Course Conent

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import {
+  // addAnwser,
+  // addQuestion,
+  // addReplyToReview,
+  // addReview,
+  // deleteCourse,
   editCourse,
-  uploadCourse,
-  getSingleCourse,
+  // generateVideoUrl,
+  // getAdminAllCourses,
   getAllCourses,
+  getCourseByUser,
+  getSingleCourse,
+  uploadCourse,
 } from "../controllers/course.controller";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const courseRouter = express.Router();
@@ -23,5 +31,38 @@ courseRouter.put(
 );
 
 courseRouter.get("/get-course/:id", getSingleCourse);
+
 courseRouter.get("/get-courses", getAllCourses);
+
+// courseRouter.get(
+//   "/get-admin-courses",
+//   isAutheticated,
+//   authorizeRoles("admin"),
+//   getAdminAllCourses
+// );
+
+courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
+
+// courseRouter.put("/add-question", isAutheticated, addQuestion);
+
+// courseRouter.put("/add-answer", isAutheticated, addAnwser);
+
+// courseRouter.put("/add-review/:id", isAutheticated, addReview);
+
+// courseRouter.put(
+//   "/add-reply",
+//   isAutheticated,
+//   authorizeRoles("admin"),
+//   addReplyToReview
+// );
+
+// courseRouter.post("/getVdoCipherOTP", generateVideoUrl);
+
+// courseRouter.delete(
+//   "/delete-course/:id",
+//   isAutheticated,
+//   authorizeRoles("admin"),
+//   deleteCourse
+// );
+
 export default courseRouter;
