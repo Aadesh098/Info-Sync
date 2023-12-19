9da59a6a73ea1563ae659d83d0548a1b74c3d33e
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-12-19 09:21:04 +0530

    Create-Order

--- server/app.ts
+++ server/app.ts
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
-// import orderRouter from "./routes/order.route";
+import orderRouter from "./routes/order.route";
 // import notificationRouter from "./routes/notification.route";
 // import analyticsRouter from "./routes/analytics.route";
 // import layoutRouter from "./routes/layout.route";
@@ -38,7 +38,7 @@ const limiter = rateLimit({
 app.use(
   "/api/v1",
   userRouter,
-  // orderRouter,
+  orderRouter,
   courseRouter,
   // notificationRouter,
   // analyticsRouter,
