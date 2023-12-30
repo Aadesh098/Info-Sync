bb0036c70e6c01075003041cb29b1d6312a3f470
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-12-30 06:42:04 +0530

    Kinda Heavy Workload

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// @type {import('next').NextConfig}
+const nextConfig = {
+    images: {
+      domains: ['res.cloudinary.com', 'randomuser.me'],
+    },
+    experimental: {
+      reactRoot: true,
+      suppressHydrationWarning: true,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
